a048c86e527bbed68beb6cdf62f7ecc925fe78ea
Andrew Kulikov <jadecoresky@gmail.com>
2022-05-22 21:56:07 +0300

    feat: implement get_all_balancer_pools func

---
+++ aura_voter/constants.py
@@ -0,0 +1,2 @@
+# Used URLs
+BALANCER_GQL_URL = "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2"
---
+++ aura_voter/data_collectors/graph_collectors.py
@@ -0,0 +1,34 @@
+from typing import Dict
+from typing import List
+from typing import Optional
+
+from gql import gql
+
+from aura_voter.constants import BALANCER_GQL_URL
+from aura_voter.data_collectors.transports import make_gql_client
+
+GET_BAL_POOLS_QUERY = """
+query {{
+  pools(first: {first}, skip: {skip}) {{
+    id
+    address
+    poolType
+  }}
+}}
+"""
+
+
+def get_all_balancer_pools() -> Optional[List[Dict]]:
+    """
+    """
+    client = make_gql_client(BALANCER_GQL_URL)
+    all_pools = []
+    limit = 100
+    offset = 0
+    while True:
+        result = client.execute(gql(GET_BAL_POOLS_QUERY.format(first=limit, skip=offset)))
+        all_pools.extend(result['pools'])
+        if not result or len(result['pools']) < limit - 1:
+            break
+        offset += limit
+    return all_pools
