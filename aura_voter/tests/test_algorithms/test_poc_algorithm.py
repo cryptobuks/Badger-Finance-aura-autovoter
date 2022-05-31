3e5eb3c4e9ad4eea82330e5e2bea6317a1c1e3aa
Andrew Kulikov <jadecoresky@gmail.com>
2022-05-31 15:17:31 +0300

    feat: more calculation testing for algo

--- aura_voter/tests/test_algorithms/test_poc_algorithm.py
+++ aura_voter/tests/test_algorithms/test_poc_algorithm.py
@@ -1,10 +1,13 @@
 from decimal import Decimal
 
+import pytest
+
 from aura_voter.data_collectors import PoolBalance
 from aura_voter.voting_algorithms.poc_algorithm import POCVoter
 
 
 def test_poc_algorithm_happy_simple_data():
+    # TODO: When aura launches, rewrite this test with real test data
     locked_aura = Decimal(1000)
     balances = [
         PoolBalance(
@@ -33,3 +36,28 @@ def test_poc_algorithm_happy_simple_data():
     }
     # Make sure all votes make 100% when summed up
     assert sum(votes.values()) == Decimal(100)
+
+
+@pytest.mark.parametrize(
+    "balance",
+    [
+        Decimal(1), Decimal(900), Decimal(0.00123)
+    ]
+)
+def test_poc_algorithm_calc_comparison(balance):
+    locked_aura = Decimal(1000)
+    voter = POCVoter(
+        locked_aura, [PoolBalance(
+            pool_id="some_pool",
+            balance=balance,
+            target_token="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        )]
+    )
+    votes = voter.propose_voting_choices()
+    pool_expected_vote = ((
+        balance * POCVoter.ALGORITHM_SETTINGS.badger_pools_fixed_vote_weight
+    ) / locked_aura) * Decimal(100)
+    assert pool_expected_vote == votes['some_pool']
+
+    badger_wbtc_expected_vote = Decimal(100) - pool_expected_vote
+    assert badger_wbtc_expected_vote == votes['badger_wbtc']
