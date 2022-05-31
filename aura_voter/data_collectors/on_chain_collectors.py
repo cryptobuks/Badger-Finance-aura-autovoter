e4bd0f0e3d6839602d7a0239df40f3d5ddd0e103
Andrew Kulikov <jadecoresky@gmail.com>
2022-05-31 16:49:39 +0300

    chore: enum cleanup

--- aura_voter/data_collectors/on_chain_collectors.py
+++ aura_voter/data_collectors/on_chain_collectors.py
@@ -1,26 +1,10 @@
 from decimal import Decimal
-from enum import Enum
 from typing import Optional
 
-from aura_voter.constants import BALANCER_VAULT_ADDRESS_ETH
+from aura_voter.constants import BALANCER_VAULT_ADDRESS
 from aura_voter.data_collectors import PoolBalance
 from aura_voter.utils import get_abi
 from aura_voter.web3 import get_web3
-
-
-class PoolType(Enum):
-    """
-    Enum of all known Balancer pool types
-    """
-    Element = "Element"
-    Weighted = "Weighted"
-    Investment = "Investment"
-    Stable = "Stable"
-    MetaStable = "MetaStable"
-    StablePhantom = "StablePhantom"
-    ERC4626Linear = "ERC4626Linear"
-    LiquidityBootstrapping = "LiquidityBootstrapping"
-    AaveLinear = "AaveLinear"
 
 
 def get_balancer_pool_token_balance(
@@ -30,7 +14,7 @@ def get_balancer_pool_token_balance(
     """
     web3 = get_web3()
     balancer_vault = web3.eth.contract(
-        address=web3.toChecksumAddress(BALANCER_VAULT_ADDRESS_ETH),
+        address=web3.toChecksumAddress(BALANCER_VAULT_ADDRESS),
         abi=get_abi("BalancerVault")
     )
     token_contract = web3.eth.contract(
