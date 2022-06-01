a5430ad385e49bbcf39cfbc018c0c792bd0c2796
Andrew Kulikov <jadecoresky@gmail.com>
2022-06-01 18:28:17 +0300

    feat: add EIP712 msg signing functionality

--- aura_voter/tests/test_utils.py
+++ aura_voter/tests/test_utils.py
@@ -1,6 +1,10 @@
+import time
+from unittest.mock import MagicMock
+
 import pytest
 
 from aura_voter.utils import get_abi
+from aura_voter.utils import sign_message
 
 
 def test_get_abi():
@@ -11,3 +15,57 @@ def test_get_abi():
 def test_get_abi_raises():
     with pytest.raises(FileNotFoundError):
         get_abi("unknown_abi")
+
+
+def test_sign_message(mocker):
+    mocker.patch(
+        "aura_voter.utils.get_web3",
+        return_value=MagicMock(eth=MagicMock(
+            account=MagicMock(
+                sign_message=MagicMock(return_value={
+                    'signature': b"\x9d\xa7\x12P\xaf\xd0\xb6'\
+                    x9b\x06\xc5\xfd/\xb9\x83y'\xb3\xff\x9e\x05\xcf"}
+                )
+            )
+        ))
+    )
+    msg = {
+        "domain": {
+            "chainId": 1,
+            "name": 'snapshot',
+            "verifyingContract": '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+            "version": '0.1.4',
+        },
+        "message": {
+            'from': "0x12d8E12e981be773cb777Be342a528228b3c7661",
+            'space': "cvx.eth",
+            'timestamp': int(time.time()),
+            'proposal': "QmetYVgwr8MXEBVg4gHNNK2D5Jre18vtJgC14VhDirh4VJ",
+            'choice': "pogger",
+            'metadata': "",
+        },
+        "primaryType": 'Vote',
+        "types": {
+            'EIP712Domain': [
+                {'name': 'name', 'type': 'string'},
+                {'name': 'version', 'type': 'string'},
+                {'name': 'chainId', 'type': 'uint256'},
+                {'name': 'verifyingContract', 'type': 'address'},
+            ],
+            "Vote": [
+                {'name': 'from', 'type': 'address'},
+                {'name': 'space', 'type': 'string'},
+                {'name': 'timestamp', 'type': 'uint64'},
+                {'name': 'proposal', 'type': 'string'},
+                {'name': 'choice', 'type': 'string'},
+                {'name': 'metadata', 'type': 'string'}
+            ],
+        },
+    }
+    signature = sign_message(
+        message=msg,
+        # Some random pk
+        private_key="123"
+    )
+    assert signature == '9da71250afd0b62720202020202020202020202020202' \
+                        '0202020202078396206c5fd2fb9837927b3ff9e05cf'
--- aura_voter/utils.py
+++ aura_voter/utils.py
@@ -4,8 +4,22 @@ from typing import Dict
 from typing import List
 from typing import Union
 
+from eth_account.messages import encode_structured_data
+
+from aura_voter.web3 import get_web3
+
 
 def get_abi(contract_name: str) -> Union[Dict, List[Dict]]:
     project_root_dir = os.path.abspath(os.path.dirname(__file__))
     with open(f"{project_root_dir}/abi/{contract_name}.json") as f:
         return json.load(f)
+
+
+def sign_message(message: Dict, private_key: str) -> str:
+    """
+    EIP712 message signing
+    """
+    web3 = get_web3()
+    encoded_data = encode_structured_data(primitive=message)
+    signed_message = web3.eth.account.sign_message(encoded_data, private_key)
+    return signed_message['signature'].hex()
