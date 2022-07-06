2d4ed0e7a0e8bbdae35f545472e7ee34118898d9
Andrew Kulikov <jadecoresky@gmail.com>
2022-07-06 19:18:28 +0300

    feat: add function to calc amount of proposals; refactor fetching proposals with limit-offset; tests

--- aura_voter/data_collectors/snapshot_collectors.py
+++ aura_voter/data_collectors/snapshot_collectors.py
@@ -14,19 +14,19 @@ from aura_voter.web3 import get_web3
 
 log.setLevel(logging.WARNING)
 
-GET_PROPOSAL_Q = gql("""
-query {
+GET_ACTIVE_PROPOSALS_Q = """
+query {{
   proposals (
-    first: 10,
-    skip: 0,
-    where: {
+    first: {first},
+    skip: {skip},
+    where: {{
       space_in: ["aurafinance.eth"],
       state: "active"
       network_in: ["1"]
-    },
+    }},
     orderBy: "created",
     orderDirection: desc
-  ) {
+  ) {{
     id
     title
     body
@@ -37,13 +37,44 @@ query {
     network
     state
     author
-    space {
+    space {{
       id
       name
-    }
-  }
-}
-""")
+    }}
+  }}
+}}
+"""
+
+
+GET_ALL_PROPOSALS_Q = """
+query {{
+  proposals (
+    first: {first},
+    skip: {skip},
+    where: {{
+      space_in: ["aurafinance.eth"],
+      network_in: ["1"]
+    }},
+    orderBy: "created",
+    orderDirection: desc
+  ) {{
+    id
+    title
+    body
+    start
+    end
+    snapshot
+    choices
+    network
+    state
+    author
+    space {{
+      id
+      name
+    }}
+  }}
+}}
+"""
 
 
 GET_SINGLE_PROPOSAL_Q = lambda snapshot_id: gql(f"""
@@ -85,23 +116,48 @@ def get_gauge_weight_snapshot() -> Optional[Dict]:
     """
     client = make_gql_client(SNAPSHOT_GQL_API_URL)
     web3 = get_web3()
-    result = client.execute(GET_PROPOSAL_Q)
-    if not result or not result.get("proposals"):
-        return
-    gauge_proposal = None
-    for proposal in result['proposals']:
-        match = re.match(r"Gauge Weight for Week of .+", proposal['title'])
-        number_of_choices = len(proposal['choices'])
-        current_timestamp = web3.eth.getBlock(web3.eth.get_block_number())['timestamp']
-        # Use heuristics to find out latest gauge proposal since there is no other way
-        # to filter out CVX proposals
-        if match and number_of_choices > SNAPSHOT_MIN_AMOUNT_POOLS:
-            # Sanity check: proposal should have been started before current date and
-            # should end after
-            if proposal['end'] > current_timestamp > proposal['start']:
-                gauge_proposal = proposal
-                break
-    return gauge_proposal
+    limit = 100
+    offset = 0
+    while True:
+        result = client.execute(gql(GET_ACTIVE_PROPOSALS_Q.format(first=limit, skip=offset)))
+        if not result or not result.get("proposals"):
+            break
+        gauge_proposal = None
+        for proposal in result['proposals']:
+            match = re.match(r"Gauge Weight for Week of .+", proposal['title'])
+            number_of_choices = len(proposal['choices'])
+            current_timestamp = web3.eth.getBlock(web3.eth.get_block_number())['timestamp']
+            # Use heuristics to find out latest gauge proposal since there is no other way
+            # to filter out CVX proposals
+            if match and number_of_choices > SNAPSHOT_MIN_AMOUNT_POOLS:
+                # Sanity check: proposal should have been started before current date and
+                # should end after
+                if proposal['end'] > current_timestamp > proposal['start']:
+                    gauge_proposal = proposal
+                    break
+        return gauge_proposal
+
+
+def get_amount_of_aura_proposals() -> Optional[int]:
+    """
+    Utility function that calculates amount of gauge weight snapshot proposals for Aura.
+    This is needed for data processing of HH bribes in other places
+    """
+    client = make_gql_client(SNAPSHOT_GQL_API_URL)
+    limit = 100
+    offset = 0
+    gauge_weight_proposal_count = 0
+    while True:
+        result = client.execute(gql(GET_ALL_PROPOSALS_Q.format(first=limit, skip=offset)))
+        offset += limit
+        if not result or not result.get("proposals"):
+            break
+        for proposal in result['proposals']:
+            match = re.match(r"Gauge Weight for Week of .+", proposal['title'])
+            if match:
+                gauge_weight_proposal_count += 1
+
+    return gauge_weight_proposal_count
 
 
 def get_snapshot_by_id(snapshot_id: str) -> Optional[Dict]:
--- aura_voter/tests/test_data_collectors/test_snapshot_collectors.py
+++ aura_voter/tests/test_data_collectors/test_snapshot_collectors.py
@@ -1,5 +1,7 @@
+from copy import deepcopy
 from unittest.mock import MagicMock
 
+from aura_voter.data_collectors.snapshot_collectors import get_amount_of_aura_proposals
 from aura_voter.data_collectors.snapshot_collectors import get_gauge_weight_snapshot
 from aura_voter.data_collectors.snapshot_collectors import get_snapshot_by_id
 from aura_voter.tests.test_data.test_data import PROPOSAL_TEST_DATA
@@ -114,3 +116,29 @@ def test_get_snapshot_by_id_empty_res(mocker):
     assert not get_snapshot_by_id(
         "0xabaf9275ae0533ce991059e8b5664225bf54bae81b9305ae60b48198db180ad9"
     )
+
+
+def test_get_amount_of_aura_proposals_happy(mocker):
+    mocker.patch(
+        'aura_voter.data_collectors.snapshot_collectors.make_gql_client',
+        return_value=MagicMock(
+            execute=MagicMock(
+                side_effect=[PROPOSAL_TEST_DATA, {}],
+            )
+        )
+    )
+    assert get_amount_of_aura_proposals() == 1
+
+
+def test_get_amount_of_aura_proposals_empty(mocker):
+    test_data = deepcopy(PROPOSAL_TEST_DATA)
+    test_data['proposals'][0]['title'] = "Some other proposal"
+    mocker.patch(
+        'aura_voter.data_collectors.snapshot_collectors.make_gql_client',
+        return_value=MagicMock(
+            execute=MagicMock(
+                side_effect=[test_data, {}],
+            )
+        )
+    )
+    assert get_amount_of_aura_proposals() == 0
